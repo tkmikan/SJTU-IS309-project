72ea2e1710e6da6532850a19d3f6b13cfd8dabc6
tkmk <109408590@qq.com>
2020-06-16 12:33:38 +0800

    preserve plain prefix \x00

--- task3/oaep.py
+++ task3/oaep.py
@@ -17,7 +17,7 @@ class RSA_OAEP_Key(RSAKey):
         else:
             self.__dict__ = key.__dict__.copy()
         self.hashalgo = hashalgo or hashlib.sha1
-        self.k0 = self.hashalgo().digest_size * 8
+        self.k0 = self.hashalgo().digest_size
 
     def __repr__(self):
         s = ', '.join(f'{c}:{hex(getattr(self, c))}' for c in 'nepqd' if getattr(self, c) is not None)
@@ -32,60 +32,61 @@ class RSA_OAEP_Key(RSAKey):
     def new(cls, nbits: int, hashalgo=None):
         return cls(key=super().new(nbits), hashalgo=hashalgo)
 
-    def H(self, num: int) -> int:
-        return b2i(self.hashalgo(i2b(num)).digest())
+    def H(self, m: int) -> int:
+        return self.hashalgo(m).digest()
 
-    def G(self, r: int, expandlen: int) -> int:
-        expanded = 0
-        while expanded.bit_length() < expandlen:
-            expanded = expanded << self.k0 | self.H(r + expanded)
-        return expanded >> expanded.bit_length() - expandlen
+    def G(self, seed: bytes, expandlen: int) -> bytes:
+        expanded = b''
+        while len(expanded) < expandlen:
+            expanded += self.H(seed + expanded)
+        return expanded[:expandlen]
 
     def encrypt(self, plain: 'int or bytes') -> 'int or bytes':
-        is_bytes = False
-        if isinstance(plain, bytes):
-            is_bytes = True
-            plain = b2i(plain)
-        elif not isinstance(plain, int):
+        is_bytes = True
+        if isinstance(plain, int):
+            if plain < 0:
+                raise ValueError('plain cannot be negative')
+            is_bytes = False
+            plain = i2b(plain)
+        elif not isinstance(plain, bytes):
             raise TypeError('plain should be int or bytes')
-        if plain < 0:
-            raise ValueError('plain cannot be negative')
-        padlen = self.nbits - self.k0 - plain.bit_length()
+        padlen = self.nbits // 8 - self.k0 - len(plain) - 1
         if padlen < 1:
             raise ValueError('plain too large')
-        padplain = (plain << 1 | 1) << padlen - 1
-        r = random.getrandbits(self.k0)
-        X = padplain ^ self.G(r, self.nbits - self.k0)
-        Y = r ^ self.H(X)
-        combined = X << self.k0 | Y
-        cipher = pow(combined, self.e, self.n)
-        if is_bytes:
-            cipher = i2b(cipher)
+        padplain = plain + b'\x01' + b'\x00' * (padlen - 1)
+        r = i2b(random.getrandbits(self.k0 * 8), self.k0)
+        X = bytes_xor(padplain, self.G(r, self.nbits // 8 - self.k0 - 1))
+        Y = bytes_xor(r, self.H(X))
+        combined = X + Y
+        cipher = super().encrypt(combined)
+        if not is_bytes:
+            cipher = b2i(cipher)
         return cipher
 
     def decrypt(self, cipher: 'int or bytes') -> 'int or bytes':
         if self.is_public:
             raise TypeError('Public key cannot decrypt')
-        is_bytes = False
-        if isinstance(cipher, bytes):
-            is_bytes = True
-            cipher = b2i(cipher)
-        elif not isinstance(cipher, int):
+        is_bytes = True
+        if isinstance(cipher, int):
+            if cipher < 0:
+                raise ValueError('cipher cannot be negative')
+            is_bytes = False
+            cipher = i2b(cipher)
+        elif not isinstance(cipher, bytes):
             raise TypeError('cipher should be int or bytes')
-        if cipher < 0:
-            raise ValueError('cipher cannot be negative')
-        if cipher > self.n:
+        if b2i(cipher) > self.n:
             raise ValueError('cipher too large')
-        combined = pow(cipher, self.d, self.n)
-        X, Y = combined >> self.k0, combined % 2 ** self.k0
-        r = Y ^ self.H(X)
-        plain = X ^ self.G(r, self.nbits - self.k0)
-        while plain & 1 == 0:
-            plain >>= 1
-        plain >>= 1
+        combined = super().decrypt(cipher)
+        X, Y = combined[:-self.k0], combined[-self.k0:]
+        r = bytes_xor(Y, self.H(X))
+        plain = bytes_xor(X, self.G(r, self.nbits // 8 - self.k0 - 1))
+        plain = plain.rstrip(b'\x00')
+        if plain[-1] != 1:
+            raise ValueError('bad padding')
+        plain = plain[:-1]
 
-        if is_bytes:
-            plain = i2b(plain)
+        if not is_bytes:
+            plain = b2i(plain)
         return plain
 
 
@@ -102,3 +103,4 @@ if __name__ == '__main__':
     print('ciphertext 2 (hex):', cipher2.hex())
     assert cipher1 != cipher2
     assert plain == privkey.decrypt(cipher1) == privkey.decrypt(cipher2)
+    assert privkey.decrypt(pubkey.encrypt(b'\x00\x00\x00\x10')) == b'\x00\x00\x00\x10'
